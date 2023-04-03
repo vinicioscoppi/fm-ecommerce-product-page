990c8c1013d6462a16c23ed2a8c0a67edd169111
Vinicios Coppi <vinicius.eller@contaazul.com>
2023-04-03 18:39:01 -0300

    setting up Vue

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+  <navbar></navbar>
+  <main>
+  <product-showcase></product-showcase>
+  <product-info></product-info>
+  </main>
+
+  Collections
+  Men
+  Women
+  About
+  Contact
+
+  Sneaker Company
+
+  Fall Limited Edition Sneakers
+
+  These low-profile sneakers are your perfect casual wear companion. Featuring a
+  durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+  $125.00
+  50%
+  $250.00
+
+  0
+  Add to cart
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-size: 16px;
+  font-family: 'Kumbh Sans', sans-serif;
+}
+</style>
