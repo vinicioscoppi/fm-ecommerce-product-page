11ec8fcd2c30b43c4fa41ab8d310016ec42d8ba8
Vinicios Coppi Eller <vcoppi.eller@gmail.com>
2023-04-30 19:42:55 -0300

    quantity picker behavior

--- src/components/QuantityPicker.vue
+++ src/components/QuantityPicker.vue
@@ -1,40 +1,37 @@
 <template>
   <div class="quantity-picker">
-    <svg
-      width="12"
-      height="4"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <defs>
-        <path
-          d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z"
-          id="a"
-        />
-      </defs>
-      <use fill-rule="nonzero" xlink:href="#a" />
-    </svg>
-    0
-    <svg
-      width="12"
-      height="12"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <defs>
-        <path
-          d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z"
-          id="b"
-        />
-      </defs>
-      <use fill-rule="nonzero" xlink:href="#b" />
-    </svg>
+    <button class="minus" @click="quantity--" :disabled="quantity <= 0">
+      <svg width="12" height="4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <path
+            d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z"
+            id="a" />
+        </defs>
+        <use fill-rule="nonzero" xlink:href="#a" />
+      </svg>
+    </button>
+    {{ quantity }}
+    <button class="plus" @click="quantity++" :disabled="quantity >= 10">
+      <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+          <path
+            d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z"
+            id="b" />
+        </defs>
+        <use fill-rule="nonzero" xlink:href="#b" />
+      </svg>
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: "QuantityPicker",
+  data() {
+    return {
+      quantity: 0
+    }
+  }
 };
 </script>
 
@@ -52,13 +49,20 @@ export default {
   font-weight: 700;
 }
 
+button {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  background: transparent;
+}
+
 svg {
   padding: 1rem;
-  fill:var(--color-orange);
+  fill: var(--color-orange);
 }
 
 svg:hover {
   cursor: pointer;
-  fill:var(--color-orange-hover);
+  fill: var(--color-orange-hover);
 }
 </style>
